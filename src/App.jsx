225b550dcaffd28fbaea528ae8e1b447791dc740
2shruti2 <shruthesharma@gmail.com>
2024-05-18 23:23:22 +0530

    feat: sentry integration for performance monitoring and error tracking

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import * as Sentry from "@sentry/react";
+
 import Hero from "./components/Hero"
 import Hightlights from "./components/Hightlights"
 import Model from "./components/Model"
 import Navbar from "./components/Navbar"
 
 const App =() => {
+
+  // return <button onClick={() => methodDoesNotExist()}>Break the world</button>;
+  
   return (
     <main className="bg-black">
       <Navbar/>
@@ -14,4 +19,4 @@ const App =() => {
   )
 }
 
-export default App
+export default Sentry.withProfiler(App);
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import * as Sentry from "@sentry/react";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import "./index.css";
+
+Sentry.init({
+  dsn: "https://92592286d457e558a289d6e135d4038a@o4507273074245632.ingest.us.sentry.io/4507273106817024",
+  integrations: [
+    Sentry.browserTracingIntegration({
+      enableInp: true,
+    }),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
